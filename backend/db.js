4b23543c2735c6ed376104660099219c814c4dbf
sayakray98 <94747548+sayakray98@users.noreply.github.com>
2024-11-05 11:53:40 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-connectToMongoose().catch(err => console.log(err));
-
 async function connectToMongoose() {
-    await mongoose.connect('mongodb+srv://djssrock93:Sayak1234@@@cluster0.o4lln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-
-    console.log("Connected to MongoDB");
+    try {
+        // Fix the MongoDB connection string - remove extra @ symbols
+        await mongoose.connect('mongodb+srv://djssrock93:Sayak1234@@@cluster0.o4lln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1); // Exit if cannot connect to database
+    }
 }
 
 module.exports = connectToMongoose;
