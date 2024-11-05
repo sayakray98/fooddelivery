030c4267f779605f67957517455845e533fe2cf3
sayakray98 <94747548+sayakray98@users.noreply.github.com>
2024-11-05 11:45:29 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 connectToMongoose().catch(err => console.log(err));
 
 async function connectToMongoose() {
-    await mongoose.connect('mongodb+srv://djssrock93:Sayak1234@@cluster0.o4lln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect('mongodb+srv://djssrock93:Sayak1234@@@cluster0.o4lln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
     console.log("Connected to MongoDB");
 }
