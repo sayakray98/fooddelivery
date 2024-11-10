7be33857cbda7f228dec7841320883cd7a0bf2bd
sayakray98 <94747548+sayakray98@users.noreply.github.com>
2024-11-10 14:17:40 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongoose = async () => {
     try {
-        await mongoose.connect('mongodb+srv://djssrock93:Sayak1234@@@cluster0.o4lln.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://djssrock93:uZgKZqnW76Pzl2lR@cluster0.o4lln.mongodb.net/Cluster0?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
