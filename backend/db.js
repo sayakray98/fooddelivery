27be1a857d432889dd2d7ba5979bb7bddc5261de
sayakray98 <94747548+sayakray98@users.noreply.github.com>
2024-11-10 13:43:57 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 // main().catch(err => console.log(err));
 const connectToMongoose = async () => {
        async function main() {
-        await mongoose.connect('mongodb://localhost:27017');
+        await mongoose.connect('mongodb+srv://djssrock93:Sayak1234@@@cluster0.o4lln.mongodb.net/Cluster0?retryWrites=true&w=majority');
         console.log("Hey Connection is Okey!")
 
     }
