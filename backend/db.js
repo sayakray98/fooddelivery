1f864e4723fa4e1bd19d3260c5d4ecb578601be8
sayakray98 <94747548+sayakray98@users.noreply.github.com>
2024-11-10 14:11:50 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,15 @@
-
-
 const mongoose = require('mongoose');
 
-// main().catch(err => console.log(err));
 const connectToMongoose = async () => {
-       async function main() {
-        await mongoose.connect('mongodb+srv://djssrock93:Sayak1234@@@cluster0.o4lln.mongodb.net/Cluster0?retryWrites=true&w=majority');
-        console.log("Hey Connection is Okey!")
-
+    try {
+        await mongoose.connect('mongodb+srv://djssrock93:Sayak1234@@@cluster0.o4lln.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connection to MongoDB is successful!");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
     }
-    main().catch(err => console.log(err));
-}
+};
 
-module.exports = connectToMongoose
+module.exports = connectToMongoose;
